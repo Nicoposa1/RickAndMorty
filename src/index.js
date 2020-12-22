7d1f93ebba6215c88fa2d5fa0f3c6788684b59eb
Nicolas Posa <nicoposa57@gmail.com>
2020-12-22 17:44:08 -0300

    terminando la base del proyecto

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Logo from './images/logo.png'
+import "./styles.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function CharacterCard(props){
+  const { character } = props;
+
+  return(
+    <div className="CharacterCard" style={{ backgroundImage: `url(${character.image})` }}>
+      <div className="CharacterCard__name-container text-truncate">
+        {character.name}
+      </div>
+    </div>
+  )
+}
+
+class App extends React.Component{
+
+  state = {
+    loading: true,
+    error: null,
+    data: {
+      info: {},
+      results: []
+    },
+    nextPage: 1
+  };
+
+  componentDidMount(){
+    this.fetchCharacter()
+  }
+
+  fetchCharacter = async() =>{
+    this.setState({ loading: true, error: null })
+    try {
+      const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${this.state.nextPage}`)
+
+      const data = await response.json()
+
+      this.setState({
+        loading: false,
+        data: {
+          info: data.info,
+          results: [].concat(this.state.data.results, data.results)
+        },
+        nextPage: this.state.nextPage + 1
+      })
+
+    }catch(error){
+      this.setState({ loading: false, error: error })
+    }
+  }
+
+  render(){
+    if(this.state.error){
+      return "Error!"
+    }
+
+    return (
+      <div className="container">
+        <div className="App">
+          <img className="Logo" src={Logo} alt="Rick and Morty"/>
+          <ul className="row">
+            {this.state.data.results.map(character => (
+              <li className="col-6 col-md-3" key={character.id} >
+                <CharacterCard character={character} />
+              </li>
+            ))}
+          </ul>
+              {this.setState.loading && <p className="text-center">Loading</p> }
+              {!this.state.loading && this.state.data.info.next && (
+                <button onClick={() => this.fetchCharacter()}>Load More</button>
+              )}
+        </div>
+      </div>
+    )
+
+  }
+
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement)
